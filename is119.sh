dd5469c403fc780648ae60989b5223b4e7aa4b1d
Younghun Ko <koyo@koyo.kr>
2020-05-14 16:10:06 +0900

    Update is119.sh

--- is119.sh
+++ is119.sh
@@ -1,2 +1,2 @@
-sudo docker build --build-arg MODE=prod --build-arg DOMAIN=wargame.is119.kr --build-arg EMAIL=koyo@koyo.kr --build-arg TYPE=certbot -t="fbctf_in_prod" .
-sudo docker run -d --name fbctf -p 80:80 -p 443:443 -v ~/is119/letsencrypt:/etc/letsencrypt fbctf_in_prod
+sudo docker build --build-arg MODE=prod --build-arg DOMAIN=wargame.is119.kr --build-arg EMAIL=koyo@koyo.kr --build-arg TYPE=certbot -t="koyokr/war119:fbctf" .
+sudo docker run -d --name fbctf -p 80:80 -p 443:443 -v ~/is119/letsencrypt:/etc/letsencrypt koyokr/war119:fbctf
